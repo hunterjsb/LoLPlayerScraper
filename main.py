98497cd05c94efaa873c5c08fc0f620edb47a5df
Hunter Boyd <admin@xanco.in>
2022-04-17 15:35:43 -0400

    finished player scraper <3

--- main.py
+++ main.py
@@ -5,55 +5,11 @@ from selenium.common.exceptions import TimeoutException
 
 from webdriver_manager.chrome import ChromeDriverManager
 
+import time
 import json
 
-
-team_dict = {
-    "G2": "G2",
-    "FNATIC": "FNC",
-    "VITALITY": "VIT",
-    "EXCEL": "XL",
-    "MISFITS": "MSF",
-    "ROGUE": "RGE",
-    "ASTRALIS": "AST",
-    "MAD": "MAD",
-    "SK": "SK",
-    "BDS": "BDS",
-    "LIQUID": "TL",
-    "TSM": "TSM",
-    "100": "100",
-    "CLG": "CLG",
-    "DIGNITAS": "DIG",
-    "EVIL": "EG",
-    "FLYQUEST": "FLY",
-    "GOLDEN": "GG",
-    "IMMORTALS": "IMT",
-    "CLOUD9": "C9"
-}
-role_dict = {
-    "SUPPORT": "SUP",
-    "BOT": "ADC",
-    "MID": "MID",
-    "JUNGLER": "JG",
-    "TOP": "TOP"
-}
-region_dict = {
-    "EUEUROPE": "EU",
-    "NANORTH": "NA",
-    "TRTURKEY": "TR",
-    "CISCIS": "CIS",
-    "KRKOREA": "KR",
-    "CNCHINA": "CN",
-    "OCEOCEANIA": "OCE",
-    "LATLATIN": "LAT",
-    "LASLAS": "LAT",
-    "BRIL": "BR",
-    "JPJAPAN": "JP",
-    "PCSPCS": "PCS",
-    "LMSLMS": "PCS",
-    "VNVIETNAM": "VN",
-    "SEASEA": "VN",
-}
+with open('resource/fandom_attributes.json') as _:
+    team_dict, role_dict, region_dict = json.load(_)
 
 
 class LoLPlayerScraper:
@@ -67,7 +23,7 @@ class LoLPlayerScraper:
         with open("resource/players.json") as f:
             self.players = json.load(f)
 
-    def get_player_stats(self, player_name: str):
+    def get_player_stats(self, player_name: str, close_driver=True):
         """
         take in a pro player's IGN and get their role, team, residency and return it as a dict
         Player's IGN capitalization is often important!
@@ -75,31 +31,65 @@ class LoLPlayerScraper:
         # open the player's page
         fandom_wiki_base_url = 'https://lol.fandom.com/wiki/'
         self.driver.set_page_load_timeout(5)  # fucking fortnite.
-        try:
+        try:  # TODO: refactor
             self.driver.get(fandom_wiki_base_url + player_name)
         except TimeoutException:
             self.driver.execute_script("window.stop();")
 
         # find it on the page and clean make the table into a list of words
-        table = self.driver.find_element(By.ID, 'infoboxPlayer').text.split()
-        table = list(filter('\u2060'.__ne__, table))  # remove occurrences of "word join" special character
-        table = table[::-1]  # reverse the table, so we get last occurrence of 'Team'
+        player_stats = self.driver.find_element(By.ID, 'infoboxPlayer').text.split()
+        player_stats = list(filter('\u2060'.__ne__, player_stats))  # remove occurrences of "word join" character
+        player_stats = player_stats[::-1]  # reverse the table, so we get last occurrence of 'Team'
 
-        # build the player dict
-        team_str = table[table.index('Team') - 1]
+        # open tournament page TODO: (refactor)
+        try:
+            self.driver.get(fandom_wiki_base_url + player_name + '/Tournament_Results')
+        except TimeoutException:
+            self.driver.execute_script("window.stop();")
+
+        time.sleep(5)  # secret tech
+        tournament_results = self.driver.find_element(By.ID, "template-reload-1").text.lower().split()
+        tournament_results = list(filter('\u2060'.__ne__, tournament_results))  # remove occurrences of \u2060
+        appearances = tournament_results.count('msi') + tournament_results.count('worlds')
+
+        # get domestic titles (god save us) (god save the queen)
+        d_titles = 0
+        for i, word in enumerate(tournament_results):
+            if word.startswith('20') and len(word) == 10:
+                place = tournament_results[i+1]
+                if place == '1':
+                    if 'playoffs' in tournament_results[i+1:i+10] or 'showdown' in tournament_results[i+1:i+10]\
+                            or 'championship' in tournament_results[i+1:i+10]:
+                        if 'lec' in tournament_results[i+1:i+10] or 'lcs' in tournament_results[i+1:i+10] \
+                                or 'lck' in tournament_results[i+1:i+10] or 'lpl' in tournament_results[i+1:i+10]:
+                            d_titles += 1
+                    if 'champions' in tournament_results[i+1:i+10] and 'preseason' not in tournament_results[i+1:i+10]:
+                        d_titles += 1
+
+        # build the player dictionary
+        if 'Team' in player_stats:
+            team_str = player_stats[player_stats.index('Team') - 1]
+        else:
+            team_str = 'FA'
         team = team_dict[team_str.upper()] if team_str.upper() in team_dict else team_str  # my shame
-        role_str = table[table.index('Role') - 1]
-        role = role_dict[role_str.upper()]
-        res_str = table[table.index('Residency') - 1]
+        role_str = player_stats[player_stats.index('Role') - 1]
+        role = role_dict[role_str.upper()] if role_str.upper() in role_dict else role_str
+        res_str = player_stats[player_stats.index('Residency') - 1]
         res = region_dict[res_str.upper()]
+
+        if close_driver:
+            self.driver.quit()
+
         return {
             'player': player_name,
             'role': role,
             'team': team,
-            'residency': res
+            'residency': res,
+            'appearances': appearances,
+            'domestic titles': d_titles
         }
 
 
 if __name__ == "__main__":
     scraper = LoLPlayerScraper()
-    print(scraper.get_player_stats('Levi'))
+    print(scraper.get_player_stats('perkz'))
