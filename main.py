caeef4c2e0de6724dd6203ea880dc1c44ad38e64
Eric "iPogoZ" Daley <ericdaley6@gmail.com>
2022-04-19 22:28:39 -0400

    Refactored if statements and updated wait function

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from selenium import webdriver  # WebDriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait  # RIP secret tech
+from selenium.webdriver.support import expected_conditions as EC
 
 from webdriver_manager.chrome import ChromeDriverManager
 
-import time
 import json
 
 with open('resource/fandom_attributes.json') as _:
@@ -28,10 +29,10 @@ class LoLPlayerScraper:
         take in a pro player's IGN and get their role, team, residency and return it as a dict
         Player's IGN capitalization is often important!
         """
-        # open the player's page
+        # open the player's page (Ideally Perkz)
         fandom_wiki_base_url = 'https://lol.fandom.com/wiki/'
         self.driver.set_page_load_timeout(5)  # fucking fortnite.
-        try:  # TODO: refactor
+        try:
             self.driver.get(fandom_wiki_base_url + player_name)
         except TimeoutException:
             self.driver.execute_script("window.stop();")
@@ -41,29 +42,31 @@ class LoLPlayerScraper:
         player_stats = list(filter('\u2060'.__ne__, player_stats))  # remove occurrences of "word join" character
         player_stats = player_stats[::-1]  # reverse the table, so we get last occurrence of 'Team'
 
-        # open tournament page TODO: (refactor)
         try:
             self.driver.get(fandom_wiki_base_url + player_name + '/Tournament_Results')
         except TimeoutException:
             self.driver.execute_script("window.stop();")
 
-        time.sleep(5)  # secret tech
-        tournament_results = self.driver.find_element(By.ID, "template-reload-1").text.lower().split()
+        # we could use this instead of the try accept blocks too
+        tournament_results = WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.ID,
+                                                                                                  "template-reload-1")))
+        tournament_results = tournament_results.text.lower().split()
         tournament_results = list(filter('\u2060'.__ne__, tournament_results))  # remove occurrences of \u2060
         appearances = tournament_results.count('msi') + tournament_results.count('worlds')
 
-        # get domestic titles (god save us) (god save the queen)
+        # get domestic titles (god save us and god save the queen)
+        playoffs = ['playoffs', 'showdown', 'championship']
+        regions = ['lec', 'lcs', 'lck', 'lpl']
         d_titles = 0
         for i, word in enumerate(tournament_results):
             if word.startswith('20') and len(word) == 10:
                 place = tournament_results[i+1]
                 if place == '1':
-                    if 'playoffs' in tournament_results[i+1:i+10] or 'showdown' in tournament_results[i+1:i+10]\
-                            or 'championship' in tournament_results[i+1:i+10]:
-                        if 'lec' in tournament_results[i+1:i+10] or 'lcs' in tournament_results[i+1:i+10] \
-                                or 'lck' in tournament_results[i+1:i+10] or 'lpl' in tournament_results[i+1:i+10]:
+                    next_ten_words = tournament_results[i + 1:i + 10]  # look at the next ten words
+                    if any(_ in next_ten_words for _ in playoffs):
+                        if any(_ in next_ten_words for _ in regions):
                             d_titles += 1
-                    if 'champions' in tournament_results[i+1:i+10] and 'preseason' not in tournament_results[i+1:i+10]:
+                    if 'champions' in next_ten_words and 'preseason' not in next_ten_words:
                         d_titles += 1
 
         # build the player dictionary
