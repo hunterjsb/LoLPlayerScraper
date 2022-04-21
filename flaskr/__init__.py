de30b30412d74cb87f3c5df88975d43b31de6dfb
thomasjohn314 <johncameronthomas@icloud.com>
2022-04-21 14:32:54 -0400

    Revert "Merge remote-tracking branch 'origin/feature/front-end-36' into feature/REST-API-56"
    This reverts commit 8097b0fe3b0805ed10fc8f6c8f070d0c99204ddc, reversing
    changes made to 3709557d969ee6e456380b8484e98f3089294f4b.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,11 +1,10 @@
 from flask import Flask
 
 from flaskr.api import api_blueprint
-from flaskr.front_end import home_blueprint, about_blueprint
+from flaskr.front_end import front_end_blueprint
 
 app = Flask(__name__)
 app.register_blueprint(api_blueprint)
-app.register_blueprint(home_blueprint)
-app.register_blueprint(about_blueprint)
+app.register_blueprint(front_end_blueprint)
 
 # don't define any routes here, create a new blueprint if needed
