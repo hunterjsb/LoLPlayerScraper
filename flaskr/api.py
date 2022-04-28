451b5beff36e97e3d7a05970396ddc685ef358e0
thomasjohn314 <johncameronthomas@icloud.com>
2022-04-28 08:54:32 -0400

    Fixed API problems, API logic is stored in a new file

--- flaskr/api.py
+++ flaskr/api.py
@@ -1,49 +1,10 @@
 from flask import Blueprint
-from json import dumps
-from tinydb import TinyDB, Query
-from datetime import date
 
-from flaskr.bs4scraper import LoLPlayerScraper
+from flaskr import api_utils
 
 api_blueprint = Blueprint('api_blueprint', __name__)
 
-player_db = TinyDB('resource/player_db.json')
-
 
 @api_blueprint.route('/api/get_player_data/<name>')
-def get_player_data(name, update_age=7):
-
-    player = Query()
-    instances = player_db.search(player.player == name)
-    num_of_instances = len(instances)
-
-    if num_of_instances < 1:
-        scraper = LoLPlayerScraper()
-        player_data = scraper.get_player_stats(name)
-        player_db.insert(player_data)
-        return dumps(player_data)
-
-    elif num_of_instances == 1:
-
-        if 'last_updated' not in instances[0]:
-            scraper = LoLPlayerScraper()
-            player_data = scraper.get_player_stats(name)
-            player_db.insert(player_data)
-            # fixing old records, remove after all old records are fixed
-            player_db.update(player_data, player.player == name)
-            return dumps(player_data)
-
-        return dumps(instances[0])
-
-    elif (date.today() - date(*instances[0]['last_updated'])).days >= update_age:
-        scraper = LoLPlayerScraper()
-        player_data = scraper.get_player_stats(name)
-        player_db.update(player_data, player.player == name)
-        return dumps(player_data)
-
-    elif num_of_instances > 1:
-
-        raise IndexError("Multiple instances of player found. Something has gone very wrong.")
-
-    return dumps(instances[0])
-    
+def get_player_data(name):
+    return api_utils.get_player_data(name)
---
+++ flaskr/api_utils.py
@@ -0,0 +1,45 @@
+from json import dumps
+from tinydb import TinyDB, Query
+from datetime import date
+
+from flaskr.bs4scraper import LoLPlayerScraper
+
+def get_player_data(name, update_age=7):
+
+    player_db = TinyDB('resource/player_db.json')
+    player = Query()
+    instances = player_db.search(player.player == name)
+    num_of_instances = len(instances)
+
+    if num_of_instances < 1:
+        scraper = LoLPlayerScraper()
+        player_data = scraper.get_player_stats(name)
+        player_db.insert(player_data)
+        player_db.close()
+        return dumps(player_data)
+
+    elif num_of_instances == 1:
+        if 'last_updated' not in instances[0]:
+            scraper = LoLPlayerScraper()
+            player_data = scraper.get_player_stats(name)
+            player_db.insert(player_data)
+            # fixing old records, remove after all old records are fixed
+            player_db.update(player_data, player.player == name)
+            player_db.close()
+            return dumps(player_data)
+
+        player_db.close()
+        return dumps(instances[0])
+
+    elif (date.today() - date(*instances[0]['last_updated'])).days >= update_age:
+        scraper = LoLPlayerScraper()
+        player_data = scraper.get_player_stats(name)
+        player_db.update(player_data, player.player == name)
+        player_db.close()
+        return dumps(player_data)
+
+    elif num_of_instances > 1:
+        player_db.close()
+        raise IndexError('Multiple instances of player found. Something has gone very wrong.')
+
+    return dumps(instances[0])
