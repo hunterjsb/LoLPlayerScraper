0caa4bad3121c7e21261c7ca129712f595926f17
Hunter Boyd <admin@xanco.in>
2022-06-05 14:55:00 -0400

    added api test

--- test/api_test.py
+++ test/api_test.py
@@ -8,6 +8,10 @@ class APITest(unittest.TestCase):
         player_data = requests.get('http://0.0.0.0:8000/api/get_player/perkz')
         self.assertEqual(player_data.json()['player'], 'perkz', 'Names not equal.')
 
+    def test_get_team(self):
+        team_data = requests.get('http://0.0.0.0:8000/api/get_team/TL')
+        self.assertEqual(team_data.json()['name'], 'TL', 'Names not equal.')
+
 
 if __name__ == "__main__":
     unittest.main()
